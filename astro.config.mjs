e2e6bfe3732d168a5e14547a1592ededbcac32e9
Tor <torhallemcintyre@gmail.com>
2025-02-04 17:51:52 +1300

    Domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://www.torhm.com',
+	site: 'https://torhm.com',
 	integrations: [mdx(), sitemap()],
 });
